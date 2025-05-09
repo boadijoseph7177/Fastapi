e88ecdd413b47992b4f419e01daccd67dd9383e9
boadijoseph7177 <boadi_j@live.bluefieldstate.edu>
2025-05-09 13:20:01 -0400

    Added password encryption

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI, HTTPException, Response, status, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
-from typing import Optional
+from typing import Optional, List
 import psycopg2
+from passlib.context import CryptContext
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from app import models
+from app import models, schemas
 from app.database import engine,get_db
 
 
@@ -15,13 +16,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    id: Optional[int] = None
-
+    
 while True:
     try: 
         conn = psycopg2.connect(host = 'localhost', database='fastapi', user='postgres', 
@@ -38,18 +33,14 @@ while True:
 def root():
     return {"message": "Hello World"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.ResponsePost])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 @app.post("/posts")
-def create_post(post: Post, db: Session = Depends(get_db)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
     #     (post.title, post.content, post.published))
 
@@ -61,7 +52,7 @@ def create_post(post: Post, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}" , response_model=schemas.ResponsePost)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, str(id))
     # post = cursor.fetchone()
@@ -71,7 +62,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
             status_code= 404,
             detail = f"The post with id={id} was not found."
         )
-    return {"details": post}
+    return post
 
 
 @app.delete("/posts/{id}")
@@ -91,19 +82,40 @@ def delete_post(id: int, db: Session = Depends(get_db)):
    
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+@app.put("/posts/{id}" , response_model=schemas.ResponsePost)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
 
-    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE 
-    id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE 
+    # id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if updated_post == None:
+    if post == None:
         raise HTTPException(
             status_code = 404,
             detail=f"The post with id={id} was not found."
         )
     
-    return f"The post with id {id} has been successfully updated."
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    
+    return post_query.first()
+
+
+@app.get("/users", response_model=List[schemas.UserOut])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@app.post("/users", response_model = schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
---
+++ app/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(Post):
+    pass
+
+class ResponsePost(Post):
+    id: int
+    created_at: datetime
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    name: str
+    password: str
+    email: EmailStr
+
+class UserOut(BaseModel):
+    id: int
+    created_at: datetime
+    email: EmailStr
+    name: str
+    class Config:
+        orm_mode = True
+
+    
