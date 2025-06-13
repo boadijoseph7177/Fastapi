a8bed7d086d91bbc89db23308e90954855bedb58
boadijoseph7177 <boadi_j@live.bluefieldstate.edu>
2025-06-13 18:16:02 -0400

    Linked users to posts

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -21,10 +21,8 @@ def get_posts( db: Session = Depends(get_db), current_user: models.User =
 @router.post("/", response_model=schemas.ResponsePost)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: models.User =
                 Depends(Oauth2.get_current_user)):
-    
-    print(current_user.email)
 
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
